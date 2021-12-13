c8b38c6994219862177cbee9f2c4a2154bc8116a
simon foucher <simon@fond.co>
2021-12-13 17:29:30 -0500

    change to type for validation erros

--- app.rb
+++ app.rb
@@ -1,24 +1,22 @@
 require 'rubygems'
-require 'sinatra/base'
 require 'sinatra'
 require 'pry'
 require_relative 'models/home.rb'
 
+get '/' do
+  erb :index
+end
 
-  get '/' do
-    erb :index
+post '/create' do
+  address = params['address']
+  @home = Home.new(address)
+
+  if !@home.valid?
+    erb :error
+  elsif @home.valid? && @home.owner.nil?
+    @home.errors.add :base, "address not in pdx database"
+    erb :error
+  else
+    erb :create
   end
-
-  post '/create' do
-    address = params['address']
-    @home = Home.new(address)
-
-    if !@home.valid?
-      erb :error
-    elsif @home.valid? && @home.owner.nil?
-      @home.errors.add :base, "address not in pdx database"
-      erb :error
-    else
-      erb :create
-    end
-  end
+end
